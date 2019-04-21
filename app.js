d319566661db64e0e70ef531da49eeda00ff3068
gggllm <515815530@qq.com>
2019-04-21 13:16:41 -0400

    fix bug in dropping filter only columns

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const buffer_size = block_size * 4;
 // 6000000 can pass small
 // still need to pause the stream for fs await to work
 
-const MAX_ROW = 600000;
+const MAX_ROW = 500000;
 
 let builtFlag = false;
 const letter = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
@@ -84,7 +84,8 @@ function build(path, tableName) {
     let uniqueArray = [];
     //261732673
     // 561732673
-    if (fs.statSync(path).size < 151732673) {
+    // for table smaller then 2mb, store them in memory
+    if (fs.statSync(path).size < 2000000) {
         let ds = [];
         inMemoryDataBase[tableName] = ds;
         let cur = [];
